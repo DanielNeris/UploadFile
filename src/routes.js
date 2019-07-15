08ff207f49831bb0f4a4d5726bc5b6eb45c12732
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-07-15 12:41:53 -0300

    config multer

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 routes.get('/', (req, res) => {
     return res.json({ hello: "World!" });
+});
+
+routes.post('/post', multer(multerConfig).single('file'), (req, res) => {
+    
 });
 
 module.exports = routes;
